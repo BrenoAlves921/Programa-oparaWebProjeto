04336ec265a2f2e0db49b8aa8005c4c30d4e2228
Tomazzlopes <thomaslopes2007@gmail.com>
2026-05-13 12:02:49 +0100

    Desenvolvimento APIIS

---
+++ data/api.js
@@ -0,0 +1,59 @@
+const apikey = "140535bf2db1a720726c59b81e9ca249"
+
+async function procurarDrivers(nomedriver) {
+  try {
+    const response = await fetch(
+      "https://v1.formula-1.api-sports.io/${encodeURIComponent(nomedriver)}", // envia um pedido á api com o nomepesquisado
+      {
+        method: "GET",
+        headers: {
+        "x-apisports-key": apikey
+        }
+      }
+    );
+    const dados = await response.json();  // espera pela resposta da api e converte para json
+    console.log(dados);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+
+async function procurarEquipa(nomeequipa) {
+  try {
+    const response = await fetch(
+      "https://v1.formula-1.api-sports.io/${encodeURIComponent(nomeequipa)}", // envia um pedido á api com o nomepesquisado
+      {
+        method: "GET",
+        headers: {
+        "x-apisports-key": apikey
+        }
+      }
+    );
+    const dados = await response.json();  // espera pela resposta da api e converte para json
+    console.log(dados);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+
+
+async function procurarcorridas() {
+  try {
+    const response = await fetch(
+      "https://v1.formula-1.api-sports.io/races", // envia um pedido á api com o nomepesquisado
+      {
+        method: "GET",
+        headers: {
+        "x-apisports-key": apikey
+        }
+      }
+    );
+    const dados = await response.json();  // espera pela resposta da api e converte para json
+    console.log(dados);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
